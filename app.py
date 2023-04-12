6b98b675c4a39026681bda6d4f1c346ba3a760d9
Bart Verweire <bart.verweire@gmail.com>
2023-04-12 09:08:22 +0200

    Busy indicator (nanobar)

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 from shiny import App, render, ui
 from shinycomponents.busyindicator import *
+import time
 
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
-    busyindicator_deps(),
-    ui.div(
-        class_="shinybusy-busy"
-    )
+    ui.input_action_button("calculate", label="Long Calculation"),
+    busybar(color="#FF0000", type="manual")
 )
 
 
 def server(input, output, session):
-    @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @reactive.Effect
+    @reactive.event(input.calculate)
+    async def start_busy_bar():
+        await update_busy_bar(0, session)
+        for i in range(10):
+            print(f"Waiting step {i} ")
+            time.sleep(1)
+            await update_busy_bar((i + 1) * 10, session)
+
+    # @output
+    # @render.text
+    # def txt():
+    #     input.calculate()
+    #     time.sleep(5)
+    #     return f"n*2 is {input.n() * 2}"
+
 
 app = App(app_ui, server)
+
+def main():
+    run_app(app)
+
+if __name__ == "__main__":
+    main()
--- shinycomponents/busyindicator.py
+++ shinycomponents/busyindicator.py
@@ -1,4 +1,6 @@
+from shiny import *
 from htmltools import HTMLDependency
+
 
 def busyindicator_deps() -> HTMLDependency:
     return HTMLDependency(
@@ -9,4 +11,42 @@ def busyindicator_deps() -> HTMLDependency:
             {"src": "js/busy.js"}
         ],
         stylesheet={"href": "css/shinybusy.css"},
+    )
+
+def busyindicator():
+    return ui.TagList(
+        busyindicator_deps(),
+        ui.div(
+            class_="shinybusy-busy"
+        )
+    )
+
+def busybar(centered=False, color="#112446", height="8px", type="auto"):
+    classname="shinybusy-nanobar"
+    if centered:
+        classname="shinybusy-nanobar-centered"
+
+
+    return ui.TagList(
+        busyindicator_deps(),
+        ui.tags.style(f".{classname} .bar {{background: {color} ; height: {height};}}"),
+        ui.tags.script(
+            f'{{"timeout":1000,"mode":"nanobar","classname":"{classname}","type":"{type}"}}',
+            {
+                "type":"application/json",
+                "data-for": "shinybusy"
+            },
+            _add_ws=False
+        ),
+        ui.div(
+            class_="shinybusy-busy"
+        )
+    )
+
+async def update_busy_bar(value, session=session.get_current_session()):
+    await session.send_custom_message(
+        type="shinybusy-update-nanobar",
+        message={
+            "value": value
+        }
     )
