376e2e832a2081e42fbf1eed0c3b10be290ce8f3
bart.verweire@smals.be <bart.verweire@smals.be>
2023-12-27 14:15:49 +0100

    Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='shinycomponents',
-    version='0.0.1',
+    version='0.1.0',
     author='Bart Verweire',
     author_email='bart.verweire@smals.be',
     description='Connection Manager for Smals databases',
